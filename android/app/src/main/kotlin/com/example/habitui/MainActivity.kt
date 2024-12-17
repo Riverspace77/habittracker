0df4b3a5eec3a3b48a10e643073dff746b8bc148
Riverspace77 <woojuk1022gmail.com>
2024-12-17 14:37:46 +0900

    ui first commit

---
+++ android/app/src/main/kotlin/com/example/habitui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.habitui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/ui/checkbox.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CheckboxExample extends StatefulWidget {
+  const CheckboxExample({super.key});
+
+  @override
+  State<CheckboxExample> createState() => _CheckboxExampleState();
+}
+
+class _CheckboxExampleState extends State<CheckboxExample> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<WidgetState> states) => Colors.white;
+
+    return Checkbox(
+      checkColor: Colors.black,
+      fillColor: WidgetStateProperty.resolveWith(getColor),
+      value: isChecked,
+      onChanged: (bool? value) {
+        setState(() {
+          isChecked = value!;
+        });
+      },
+    );
+  }
+}
